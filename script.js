1ec90a488d0f2760d8575abb43ad2453e6f1d3d8
Horakal <felipe.reis.e@gmail.com>
2022-01-28 16:22:37 -0300

    added ui to the game

--- script.js
+++ script.js
@@ -1,61 +1,83 @@
+let plPoints = 0;
+let cpuPoints = 0;
+let tie = 0;
+const attRound = document.querySelector('p.round');
+const p1PointView = document.querySelector('p.p1Points');
+const cpuPointView = document.querySelector('p.cpuPoints');
+const tiePointView = document.querySelector('p.Draws');
+const numRound = document.querySelector('p.numRounds');
+const win = document.querySelector('p.win');
+
 function computerPlay(){
 	const computerPlays = ['Rock', 'Paper', 'Scissors'];
-	const randomElement = computerPlays[Math.floor(Math.random() * computerPlays.length)];
-
+	let randomElement = computerPlays[Math.floor(Math.random() * computerPlays.length)];
 	return randomElement;
 }
 
-function playerPlay(){
-	const playerMove = prompt("Make your move (Rock, Paper or Scissors): ");
-	const upperr = playerMove.toUpperCase();
-
-	if (upperr != 'ROCK' && upperr != 'PAPER' && upperr != 'SCISSORS'){
-		alert("Insert a valid value Rock, paper or scissors")
-		playerPlay();
-	}
-
-	return upperr;
-}
-
-function gameDecider(pMv, cpuMv){
-	if (pMv === 'ROCK' && cpuMv === 'Scissors' || pMv === 'SCISSORS' && cpuMv == 'Paper' || pMv === 'PAPER' && cpuMv === 'Rock')
+function playRound(pMv, cpuMv){
+	if (pMv === 'Rock' && cpuMv === 'Scissors' || pMv === 'Scissors' && cpuMv 		== 'Paper' || pMv === 'Paper' && cpuMv === 'Rock')
 		return 'player'
-	else if (cpuMv === 'Rock' && pMv === 'SCISSORS' || cpuMv === 'Scissors' && pMv == 'PAPER' || cpuMv === 'Paper' && pMv === 'ROCK')
+	else if (cpuMv === 'Rock' && pMv === 'Scissors' || cpuMv === 'Scissors' && 		pMv == 'Paper' || cpuMv === 'Paper' && pMv === 'Rock')
 		return 'cpu'
 	else
-		return ('Draw')
+		return ('Tie')
 }
 
-function game(){
-	let plPoints = 0;
-	let cpuPoints = 0;
-	let draw = 0;
+function game(id){
+
 	let result;
-	let pMv ;
-	let cpuMv ;
-	for (let i = 0; i < 5; i++){
-		cpuMv = computerPlay();
-		pMv = playerPlay();
+	let cpuMv = computerPlay();
+	let p1Play = id;
 
-		result = gameDecider(pMv, cpuMv);
-		if (result == 'player'){
-			alert(`You win this round! ${pMv} beat ${cpuMv}`)
-			plPoints++;
-		}
-		else if (result == 'cpu'){
-			alert(`You lose this round! ${cpuMv} beats ${pMv}`);
-			cpuPoints++;
-		}
-		else
-			alert("Draw");
-			draw++;
+	result = playRound(p1Play, cpuMv);
+	if (result == 'player'){
+		attRound.textContent = `You win this round! ${p1Play} beats ${cpuMv}`;
+		plPoints++;
+		p1PointView.textContent = `P1 = ${plPoints}` ;
 	}
-	if (plPoints > cpuPoints){
-		console.log(`Congratulations you have won: P1 ${plPoints} x Cpu ${cpuPoints} : Draw ${draw}`)
+	else if (result == 'cpu'){
+		attRound.textContent = `You lose this round! ${cpuMv} beats ${p1Play}`;
+		cpuPoints++;
+		cpuPointView.textContent = `Cpu = ${cpuPoints}`;
 	}
-	else if (cpuPoints > plPoints){
-		console.log(`Sorry but the computer won: Cpu ${cpuPoints} x P1 ${plPoints}: Draw ${draw}`)
+	else if (result == 'Tie'){
+		attRound.textContent = ('Tie');
+		tie++;
+		tiePointView.textContent = `Draws = ${tie}`;
 	}
-	else
-		console.log("Draw")
+	numRound.textContent = `Round ${plPoints + cpuPoints + tie}`;
+	if (plPoints  == 5 || cpuPoints == 5)
+		winner(plPoints, cpuPoints, tie)
 }
+
+function winner(p1Points, cpuPoints, tiePoints){
+	if (p1Points > cpuPoints){
+		win.textContent = `Congratulations you have won: P1 ${p1Points} x Cpu ${cpuPoints} : Tie ${tiePoints}`
+	}
+	else if (cpuPoints > p1Points){
+		win.textContent = `Sorry but the computer won: Cpu ${cpuPoints} x P1 ${p1Points}: Tie ${tiePoints}`
+	}
+	disableButton();
+}
+
+function disableButton(){
+	const btn = document.querySelectorAll('button');
+	btn.forEach(elem =>{
+		elem.disabled = true;
+	})
+	const resetBtn = document.querySelector("input[type='button']");
+	resetBtn.classList.remove('hidden');
+}
+
+function reset(){
+	location.reload();
+}
+
+function start(){
+	addEventListener('click', (e) => {
+		if(e.target.id == '') return;
+		game(e.target.id);
+	})
+}
+
+start()
